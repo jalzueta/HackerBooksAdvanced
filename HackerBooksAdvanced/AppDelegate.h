2057aa176cff42105257ea8e80d9e90727f9782d
Javi Alzueta <jalzuetap@gmail.com>
2015-04-13 18:34:11 +0200

    Initial Commit

---
+++ HackerBooksAdvanced/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerBooksAdvanced
+//
+//  Created by Javi Alzueta on 13/4/15.
+//  Copyright (c) 2015 FillinGAPPs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
